23992eb2637916964b54eccdf8568527a6c423d9
Daniel Stalder <daniel_stalder@hispeed.ch>
2023-06-26 08:30:30 +0200

    add Fracture Families

--- include/TemplateTest/Fractures/Family.hpp
+++ include/TemplateTest/Fractures/Family.hpp
@@ -1,6 +1,78 @@
 #pragma once
+#include <TemplateTest/Fractures/Fracture.hpp>
+#include <memory>
 
-namespace Fractures
-{
-    
-} // namespace Fractures
+namespace Fractures {
+class Family {
+public:
+    virtual ~Family() = default;
+
+    virtual std::unique_ptr<Fracture> createFracture(
+        const Eigen::Vector2d& pos) const = 0;
+
+    virtual const std::string& getIdName() const = 0;
+
+protected:
+    template <typename... Args>
+    static std::string createIdName(Args&&... args) {
+        // concatenates all arguments which should be an unique identifier
+        std::stringstream idName;
+        ((idName << args), ...);
+        return idName.str();
+    }
+};
+
+class TempNameFamily : public Family {
+public:
+    const std::string& getIdName() const override { return m_idName; }
+
+protected:
+    TempNameFamily(std::string_view className, double lf, double af,
+                   double angle = 0.0);
+    double m_lf;
+    double m_af;
+    double m_angle;
+    std::string m_idName;
+};
+
+class ConstantFractureFamily : public TempNameFamily {
+public:
+    ConstantFractureFamily(double lf, double af, double angle = 0.0);
+
+    std::unique_ptr<Fracture> createFracture(
+        const Eigen::Vector2d& pos) const override;
+};
+
+class LinearFractureFamily : public TempNameFamily {
+public:
+    LinearFractureFamily(double lf, double af, double angle = 0.0);
+
+    std::unique_ptr<Fracture> createFracture(
+        const Eigen::Vector2d& pos) const override;
+};
+
+class QuadraticFractureFamily : public TempNameFamily {
+public:
+    QuadraticFractureFamily(double lf, double af, double angle = 0.0);
+
+    std::unique_ptr<Fracture> createFracture(
+        const Eigen::Vector2d& pos) const override;
+};
+
+class EllipticFractureFamily : public TempNameFamily {
+public:
+    EllipticFractureFamily(double lf, double af, double angle = 0.0);
+
+    std::unique_ptr<Fracture> createFracture(
+        const Eigen::Vector2d& pos) const override;
+};
+
+class OneSidedLinearFractureFamily : public TempNameFamily {
+public:
+    OneSidedLinearFractureFamily(double lf, double af, double angle = 0.0);
+
+    std::unique_ptr<Fracture> createFracture(
+        const Eigen::Vector2d& pos) const override;
+};
+
+}  // namespace Fractures
--- src/Fractures/Family.cpp
+++ src/Fractures/Family.cpp
@@ -1,2 +1,68 @@
 #include <TemplateTest/Fractures/Family.hpp>
 
+namespace Fractures {
+
+TempNameFamily::TempNameFamily(std::string_view className, double lf, double af,
+                               double angle)
+    : m_lf{lf},
+      m_af{af},
+      m_angle{angle},
+      m_idName{createIdName(className, lf, af, angle)} {}
+
+ConstantFractureFamily::ConstantFractureFamily(double lf, double af,
+                                               double angle)
+    : TempNameFamily("ConstantFractureFamily", lf, af, angle) {}
+
+std::unique_ptr<Fracture> ConstantFractureFamily::createFracture(
+    const Eigen::Vector2d& pos) const {
+    auto frac = std::make_unique<ConstantAperture>(pos, m_angle, m_lf, m_af);
+    frac->setFamilyIdName(m_idName);
+    return frac;
+}
+
+LinearFractureFamily::LinearFractureFamily(double lf, double af, double angle)
+    : TempNameFamily("LinearFractureFamily", lf, af, angle) {}
+
+std::unique_ptr<Fracture> LinearFractureFamily::createFracture(
+    const Eigen::Vector2d& pos) const {
+    auto frac = std::make_unique<LinearAperture>(pos, m_angle, m_lf, m_af);
+    frac->setFamilyIdName(m_idName);
+    return frac;
+}
+
+QuadraticFractureFamily::QuadraticFractureFamily(double lf, double af,
+                                                 double angle)
+    : TempNameFamily("QuadraticFractureFamily", lf, af, angle) {}
+
+std::unique_ptr<Fracture> QuadraticFractureFamily::createFracture(
+    const Eigen::Vector2d& pos) const {
+    auto frac = std::make_unique<QuadraticAperture>(pos, m_angle, m_lf, m_af);
+    frac->setFamilyIdName(m_idName);
+    return frac;
+}
+
+EllipticFractureFamily::EllipticFractureFamily(double lf, double af,
+                                               double angle)
+    : TempNameFamily("EllipticFractureFamily", lf, af, angle) {}
+
+std::unique_ptr<Fracture> EllipticFractureFamily::createFracture(
+    const Eigen::Vector2d& pos) const {
+    auto frac = std::make_unique<EllipticAperture>(pos, m_angle, m_lf, m_af);
+    frac->setFamilyIdName(m_idName);
+    return frac;
+}
+
+OneSidedLinearFractureFamily::OneSidedLinearFractureFamily(double lf, double af,
+
+                                                           double angle)
+    : TempNameFamily("OneSidedLinearFractureFamily", lf, af, angle) {}
+
+std::unique_ptr<Fracture> OneSidedLinearFractureFamily::createFracture(
+    const Eigen::Vector2d& pos) const {
+    auto frac =
+        std::make_unique<OneSidedLinearAperture>(pos, m_angle, m_lf, m_af);
+    frac->setFamilyIdName(m_idName);
+    return frac;
+}
+
+}  // namespace Fractures
