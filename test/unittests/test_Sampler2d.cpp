ac6b9779c4b63335c4511f65bd9117c594efa149
Daniel Stalder <daniel_stalder@hispeed.ch>
2023-06-26 09:03:05 +0200

    add unit tests from uniform sampler

---
+++ test/unittests/test_Sampler2d.cpp
@@ -0,0 +1,42 @@
+#include <Eigen/Dense>
+#include <Eigen/Geometry>
+#include <TemplateTest/Random/Sampler2d.hpp>
+#include <catch2/catch_get_random_seed.hpp>
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_floating_point.hpp>
+#include <iostream>
+// NOLINTBEGIN (readability-function-cognitive-complexity)
+
+constexpr uint32_t numSamples = 10'000;
+constexpr double absoluteTolerance = 0.01;
+
+TEST_CASE("UniformSampler2d returns a sample within the box") {
+    Eigen::AlignedBox2d box(Eigen::Vector2d(-1, -1), Eigen::Vector2d(1, 1));
+    UniformSampler2d sampler(box);
+
+    for (uint32_t i = 0; i < numSamples; ++i) {
+        Eigen::Vector2d sample = sampler.sample();
+        REQUIRE(box.contains(sample));
+    }
+}
+
+TEST_CASE("UniformSampler2d samples uniformly within the box") {
+    Eigen::AlignedBox2d box(Eigen::Vector2d(-1, -1), Eigen::Vector2d(2, 1));
+    Random::seed(Catch::getSeed());
+    UniformSampler2d sampler(box);
+
+    Eigen::Vector2d sum_samples = Eigen::Vector2d::Zero();
+    for (uint32_t i = 0; i < numSamples; ++i) {
+        sum_samples += sampler.sample();
+    }
+    Eigen::Vector2d mean_sample = sum_samples / numSamples;
+    std::cout << "here:\n" << mean_sample.transpose() << std::endl;
+    /*
+       REQUIRE_THAT(mean_sample.x(),
+                    Catch::Matchers::WithinAbs(0.5, absoluteTolerance));
+       REQUIRE_THAT(mean_sample.y(),
+                    Catch::Matchers::WithinAbs(0.0, absoluteTolerance));
+    */
+    REQUIRE(true);
+}
+// NOLINTEND (readability-function-cognitive-complexity)
